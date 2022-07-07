0c9f5fef1707e00550543e0b48a9af6ee52e0485
4gboframram <79847791+4gboframram@users.noreply.github.com>
2022-07-07 08:45:53 -0400

    update version number for new pypi release

--- setup.py
+++ setup.py
@@ -9,15 +9,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='python_gelbooru',  # How you named your package folder (MyLib)
     packages=find_packages(),  # Chose the same as "name"
-    version='0.1.1',  # Start with a small number and increase it with every change you make
+    version='0.2.0',  # Start with a small number and increase it with every change you make
     license='gpl-3.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Python-Gelbooru is an unofficial and lightweight asynchronous wrapper for the Gelbooru API.',  # Give a short description about your library
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='4gboframram',  # Type in your name
-    # author_email='FujiMakoto@users.noreply.github.com',  # Type in your E-Mail
     url='https://github.com/4gboframram/Python-Gelbooru',  # Provide either the link to your github or to your website
-    # download_url='https://github.com/FujiMakoto/pygelbooru/archive/v0.3.1.tar.gz',
     keywords=['gelbooru', 'anime', 'artwork', 'anime artwork', 'booru'],  # Keywords that define your package best
     install_requires=[
         'aiohttp',
